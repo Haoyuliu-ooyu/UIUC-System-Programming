776b961e6471fa1481a24be7351277b98974092d
Haoyu Liu <haoyul4@illinois.edu>
2021-02-11 19:44:21 +0000

    lab submission

--- perilous_pointers/part2-main.c
+++ perilous_pointers/part2-main.c
@@ -13,5 +13,53 @@
  */
 int main() {
     // your code here
+    //
+    int first = 81;
+    first_step(first);
+    //
+    int* second = malloc(sizeof(int));
+    *second = 132;
+    second_step(second);
+    free(second);
+    //
+    int third = 8942;
+    int* third_ptr = & third;
+    double_step(&third_ptr);
+    //
+    char* forth = malloc(sizeof(int) + 5);
+    *(int *)(forth + 5) = 15;
+    strange_step(forth);
+    free(forth);
+    //
+    char* fif = malloc(4*sizeof(char));
+    fif[3] = 0;
+    empty_step((void*) fif);
+    free(fif);
+    //
+    char* s2 = malloc(4*sizeof(char));
+    s2[3] = 'u';
+    two_step((void*)s2, s2);
+    free(s2);
+    //
+    char* three = malloc(5*sizeof(char));
+    three_step(three, three+2, three+4);
+    free(three);
+    //
+    char* step = malloc(4*sizeof(char));
+    step[1] = 0;
+    step[2] = 8;
+    step[3] = 16;
+    step_step_step(step, step, step);
+    free(step);
+    //
+    char g = 1;
+    it_may_be_odd(&g, (int)g);
+    //
+    char c[] = "CS241,CS241";
+    tok_step(c);
+    //
+    char i[] = {1, 2, 0, 0};
+    the_end((void*)i, (void*)i);
+    free(i);
     return 0;
 }
