921ba5304b879b5a0344260a1a6fb867423e065c
Haoyu Liu <haoyul4@illinois.edu>
2021-02-26 03:48:55 +0000

    final

--- mini_memcheck/test.c
+++ mini_memcheck/test.c
@@ -4,8 +4,13 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
     // Your tests here using malloc and free
+    void *p1 = malloc(30);
+    void * p2 = malloc(40);
+    void *p3 = realloc(p1, 20);
+    free(p2);
     return 0;
 }
