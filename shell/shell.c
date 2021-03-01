e9f207a354b23f0f2f892fea89eeb2ad48a4eba6
Haoyu Liu <haoyul4@illinois.edu>
2021-03-01 20:53:52 +0000

    save

--- shell/shell.c
+++ shell/shell.c
@@ -13,6 +13,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <fcntl.h>
+ #include <sys/stat.h>
 
 typedef struct process {
     char *command;
@@ -27,14 +29,22 @@ static FILE *source = NULL;
 
 //functions:
 int define_command(char *command);
+int is_background(vector* inputs);
 int external(char* command, vector* inputs);
 int execute_single(int cmd_id, char* cmd, int save_to_history);
+int get_process_idx(pid_t pid);
 void sig_handler(int sig);
 void shell_exit(int s);
 int push_to_history(int command_id);
 void detect_h_and_f(int argc, char* argv[]);
 char *get_directory(char *buffer);
+int add_process(char* command, pid_t pid);
 int execute(char* command);
+int operator_and(char* cmd, char* loc);
+int operator_or(char* cmd, char* loc);
+int operator_sep(char* cmd, char* loc);
+int operator_output(char* cmd, char* loc);
+int operator_append(char* cmd, char* loc);
 
 //main function
 int shell(int argc, char *argv[]) {
@@ -44,18 +54,35 @@ int shell(int argc, char *argv[]) {
     processes = shallow_vector_create();
     source = stdin;
     detect_h_and_f(argc, argv);
-
-    //
+    //add process
+    char *cmd = malloc(1);
+    cmd[0] = '\0';
+    char **curr = argv;
+    while (*curr != NULL) {
+        cmd = realloc(cmd, strlen(cmd) + strlen(*curr) + 2);
+        strcat(cmd, *curr);
+        strcat(cmd, " ");
+        ++curr;
+    }
+    cmd[strlen(cmd) - 1] = '\0';
+    add_process(cmd, getpid());
+    //shell
     size_t byte_read = 0;
     char *line = NULL;
     char *wd_buffer = malloc(256);
     print_prompt(get_directory(wd_buffer), getpid());
     while (getline(&line, &byte_read, source) != -1) {
-        if (line[strlen(line) - 1] == '\n')
-            line[strlen(line) -1] = '\0';
-        char *line_cpy = malloc(strlen(line));
-        strcpy(line_cpy, line);
-        execute(line);
+        for (size_t i = 0; i < vector_size(processes); i++) {
+            int s;
+            waitpid(((process*)vector_get(processes, i))->pid, &s, WNOHANG);
+        }
+        if (line[strlen(line) - 1] ==  '\n' && strlen(line) == 1) {}
+        else {
+            if (line[strlen(line) - 1] == '\n') {line[strlen(line) -1] = '\0';}
+            char *line_cpy = malloc(strlen(line));
+            strcpy(line_cpy, line);
+            execute(line_cpy);
+        }
         print_prompt(get_directory(wd_buffer), getpid());
         fflush(stdout);
     }
@@ -70,7 +97,7 @@ void detect_h_and_f(int argc, char* argv[]) {
     int c;
     while ((c = getopt(argc, argv, "h:f:")) != -1) {
         if (c == 'h') {
-            FILE *history_file_stream = fopen(optarg, "w");
+            FILE *history_file_stream = fopen(optarg, "a");
             if (!history_file_stream) {
                 print_history_file_error();
                 shell_exit(1);
@@ -207,7 +234,7 @@ int execute_single(int command_id, char *command, int save_to_history) {
             char *cmd = vector_get(history, i - 1);
             if (strncmp(cmd, prefix, strlen(prefix)) == 0) {
                 print_command(cmd);
-                return execute_single(define_command(cmd), cmd, save_to_history);
+                return execute(cmd);
             }
         }
         print_no_history_match();
@@ -215,10 +242,34 @@ int execute_single(int command_id, char *command, int save_to_history) {
     } else if (command_id == 5) {
         return 0;
     } else if (command_id == 6) {
-        return 0;
+       pid_t t_pid = atoi(vector_get(splited, 1));
+       size_t idx = get_process_idx(t_pid);
+       if (!idx) {
+           print_no_process_found(t_pid);
+           return 1;
+       }
+       kill(t_pid, SIGKILL);
+       print_killed_process(t_pid, ((process*)vector_get(processes, idx))->command);
+       return 0;
     } else if (command_id == 7) {
+        pid_t t_pid = atoi(vector_get(splited, 1));
+        size_t idx = get_process_idx(t_pid);
+        if (!idx) {
+           print_no_process_found(t_pid);
+           return 1;
+        }
+        kill(t_pid, SIGSTOP);
+        print_stopped_process(t_pid, ((process*)vector_get(processes, idx))->command);
         return 0;
     } else if (command_id == 8) {
+        pid_t t_pid = atoi(vector_get(splited, 1));
+        size_t idx = get_process_idx(t_pid);
+        if (!idx) {
+           print_no_process_found(t_pid);
+           return 1;
+        }
+        kill(t_pid, SIGCONT);
+        print_continued_process(t_pid, ((process*)vector_get(processes, idx))->command);
         return 0;
     } else if (command_id == 9) {
         if (size != 1) {
@@ -228,13 +279,21 @@ int execute_single(int command_id, char *command, int save_to_history) {
         shell_exit(0);
         return 0;
     } else {
-        external(vector_get(splited, 0), splited);
+        return external(vector_get(splited, 0), splited);
     }
     sstring_destroy(sstr);
     vector_destroy(splited);
     return 1;
 }
-
+int get_process_idx(pid_t proc_id) {
+    for (size_t i = 0; i < vector_size(processes); i++) {
+        process* proc = vector_get(processes, i);
+        if (proc-> pid == proc_id) {
+            return i;
+        }
+    }
+    return 0;
+}
 //execute with consideration of operation
 int execute(char* command) {
     char *loc = NULL;
@@ -245,82 +304,133 @@ int execute(char* command) {
         operator = "||";
     } else if ((loc = strstr(command, ";")) != NULL) {
         operator = ";";
+    } else if ((loc = strstr(command, ">")) != NULL) {
+        operator = ">";
+    } else if ((loc = strstr(command, ">>")) != NULL) {
+        operator = ">>";
+    } else if ((loc = strstr(command, "<")) != NULL) {
+        operator = "<";
     }
-    if (loc != NULL) {
-        memcpy(loc, "\0\0", strlen(operator));
-        char *command_1 = command;
-        char *command_2 = loc + strlen(operator);
-        int command_id_1 = define_command(command_1);
-        int command_id_2 = define_command(command_2);
-        if (strcmp(operator, "&&") == 0) {
-            if (push_to_history(command_id_1)) {
-                int retval = execute_single(command_id_1, command_1, 0);
-                if (retval == 0) {
-                    if (push_to_history(command_id_2)) {
-                        memcpy(loc, operator, strlen(operator));
-                        vector_push_back(history, command);
-                        return execute_single(command_id_2, command_2, 0);
-                    }
-                } else {
-                    memcpy(loc, operator, strlen(operator));
-                    vector_push_back(history, command);
-                    return retval;
-                }
-            }
-        } else if (strcmp(operator, "||") == 0) {
-            if (push_to_history(command_id_1)) {
-                int retval = execute_single(command_id_1, command_1, 0);
-                if (retval != 0) {
-                    if (push_to_history(command_id_2)) {
-                        memcpy(loc, operator, strlen(operator));
-                        vector_push_back(history, command);
-                        return execute_single(command_id_2, command_2, 0);
-                    }
-                } else {
-                    memcpy(loc, operator, strlen(operator));
-                    vector_push_back(history, command);
-                    return 0;
-                }
-            }
-        } else {
-            if (push_to_history(command_id_1)) {
-                execute_single(command_id_1, command_1, 0);
-                if (push_to_history(command_id_2)) {
-                    memcpy(loc, operator, strlen(operator));
-                    vector_push_back(history, command);
-                    return execute_single(command_id_2, command_2, 0);
-                }
-            }
-        }
-        memcpy(loc, operator, strlen(operator));
-        vector_push_back(history, command);
-        print_invalid_command(command);
-        return 1;
+    //
+    if (operator == NULL) {
+        return execute_single(define_command(command), command, 1);
+    } else if (!strcmp(operator, "&&")) {
+        operator_and(command, loc);
+    } else if (!strcmp(operator, "||")) {
+        operator_or(command, loc);
+    } else if (!strcmp(operator, ";")) {
+        operator_sep(command, loc);
+    } else if (!strcmp(operator, ">")) {
+        operator_output(command, loc);
+    } else if (!strcmp(operator, ">>")) {
+        operator_append(command, loc);
+    } else if (!strcmp(operator, "<")) {
+        return 0;
     } else {
         return execute_single(define_command(command), command, 1);
     }
+    return 1;
+}
+int operator_and(char* cmd, char* loc) {
+    char cmd_1[loc - cmd];
+    strncpy(cmd_1, cmd, loc - cmd);
+    cmd_1[loc - cmd -1] = '\0';
+    char* cmd_2 = loc+3;
+    int cmd_1_rel = execute(cmd_1);
+    if (!cmd_1_rel) {
+        execute(cmd_2);
+    }
+    return 0;
+}
+
+int operator_or(char* cmd, char* loc) {
+    char cmd_1[loc - cmd];
+    strncpy(cmd_1, cmd, loc - cmd);
+    cmd_1[loc - cmd - 1] = '\0';
+    char* cmd_2 = loc+3;
+    int cmd_1_rel = execute(cmd_1);
+    if (cmd_1_rel) {
+        execute(cmd_2);
+    }
+    return 0;
+}
+
+int operator_sep(char* cmd, char* loc) {
+    char cmd_1[loc - cmd + 1];
+    strncpy(cmd_1, cmd, loc - cmd);
+    cmd_1[loc - cmd] = '\0';
+    char* cmd_2 = loc+2;
+    execute(cmd_1);
+    execute(cmd_2);
+    return 0;
+}
+//
+int operator_output(char* cmd, char* loc) {
+    char cmd_1[loc - cmd + 1];
+    strncpy(cmd_1, cmd, loc - cmd);
+    cmd_1[loc - cmd] = '\0';
+    char* cmd_2 = loc+2;
+    FILE* f = fopen(cmd_2, "w+");
+    int fildes = fileno(f);
+    int saved_out = dup(fileno(stdout));
+    dup2(fildes, fileno(stdout));
+    execute_single(define_command(cmd_1), cmd_1, 1);
+    fflush(stdout);
+    close(fildes);
+    dup2(saved_out, fileno(stdout));
+    return 0;
+}
+//
+int operator_append(char* cmd, char* loc) {
+    char cmd_1[loc - cmd + 1];
+    strncpy(cmd_1, cmd, loc - cmd);
+    cmd_1[loc - cmd] = '\0';
+    char* cmd_2 = loc+2;
+    FILE* f = fopen(cmd_2, "a+");
+    int fildes = fileno(f);
+    int saved_out = dup(fileno(stdout));
+    dup2(fildes, fileno(stdout));
+    execute_single(define_command(cmd_1), cmd_1, 1);
+    fflush(stdout);
+    close(fildes);
+    dup2(saved_out, fileno(stdout));
+    return 0;
 }
 //
 //externel
 int external(char* command, vector* inputs) {
+    int background = is_background(inputs);
     pid_t pid = fork();
+    add_process(command, pid);
     if (pid > 0) {
         print_command_executed(pid);
         int status = 0;
-        if (waitpid(pid, &status, 0) == -1) {
-            print_wait_failed();
-            shell_exit(-1);
-        } else if (WIFEXITED(status)) {
-            if (WEXITSTATUS(status) != 0){
-                shell_exit(1);
+        if (background) {
+            waitpid(pid, &status, WNOHANG);
+        } else {
+            int error = waitpid(pid, &status, 0);
+            if (error == -1) {
+                print_wait_failed();
+                shell_exit(-1);
+            } else if (WIFEXITED(status)) {
+                if (WEXITSTATUS(status) != 0){
+                    shell_exit(1);
+                }
+                fflush(stdout);
+                //set_process_status(pid, STATUS_KILLED);
+            } else if (WIFSIGNALED(status)) {
+                //set_process_status(pid, STATUS_KILLED);
             }
-            fflush(stdout);
-            kill(pid, SIGKILL);
-        } else if (WIFSIGNALED(status)) {
-            kill(pid, SIGKILL);
         }
         return status;
     } else if (pid == 0) {
+        if (background) {
+            if (setpgid(getpid(), getpid()) == -1) {
+                print_setpgid_failed();
+                fflush(stdout);
+                shell_exit(1);
+            }
+        }
         fflush(stdout);
         char* argus[vector_size(inputs) + 1];
         for (size_t i = 0; i < vector_size(inputs); i++) {
@@ -337,6 +447,31 @@ int external(char* command, vector* inputs) {
         shell_exit(1);
         return 1;
     }
+}
+
+int is_background(vector* inputs) {
+    size_t size = vector_size(inputs);
+    char *last_elem = vector_get(inputs, size-1);
+    if (!strcmp(last_elem, "&")) {
+        vector_erase(inputs, size-1);
+        return 1;
+    }
+    return 0;
+}
+
+int add_process(char* command, pid_t pid) {
+    for (size_t i = 0; i < vector_size(processes); ++i) {
+        process *p = vector_get(processes, i);
+        if (p->pid == pid) {
+            p->command = command;
+            return 0;
+        }
+    }
+    process *p = malloc(sizeof(process));
+    p->command = command;
+    p->pid = pid;
+    vector_push_back(processes, p);
+    return 0;
 }
 
 //dealing with signals
