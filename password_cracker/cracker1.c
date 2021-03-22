b273aa4a57d2f4d87b5928cbaa1536f1ffcdc018
Haoyu Liu <haoyul4@illinois.edu>
2021-03-22 20:28:17 +0000

    for ag

--- password_cracker/cracker1.c
+++ password_cracker/cracker1.c
@@ -5,10 +5,94 @@
 #include "cracker1.h"
 #include "format.h"
 #include "utils.h"
+#include "./includes/queue.h"
+#include "thread_status.h"
+#include <stdio.h>
+#include <string.h>
+#include <crypt.h>
+#include <pthread.h>
+
+typedef struct cracker_t {
+    pthread_t tid;
+    size_t index;
+} cracker_t;
+
+static int recover = 0;
+static int count = 0;
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+static cracker_t* crackers = NULL;
+static queue* q = NULL;
+
+void* crack_func(void* data) {
+    cracker_t* c = (cracker_t*)data;
+    char* to_solve = NULL;
+    char username[16], hash[16], password[16];
+    while ((to_solve = queue_pull(q)) != NULL) {
+        sscanf(to_solve, "%s %s %s", username, hash, password);
+        v1_print_thread_start(c->index, username);
+        double start_time = getThreadCPUTime();
+        int hash_count = 0;
+        int found = 0;
+        if (getPrefixLength(password) == (int)strlen(password)) {
+            found = 1;
+        } else {
+            int suffix_length = (int)strlen(password) - getPrefixLength(password);
+            struct crypt_data cdata;
+            cdata.initialized = 0;
+            setStringPosition(password + getPrefixLength(password), 0);
+            int range = 1;
+            for (int i = 0; i < suffix_length; i++) {
+                range *= 26;
+            }
+            for (int i = 0; i < range; i++) {
+                hash_count++;
+                if (strcmp(crypt_r(password, "xx", &cdata), hash) == 0) {
+                    // update recovered password counter
+                    pthread_mutex_lock(&m);
+                    recover++;
+                    pthread_mutex_unlock(&m);
+                    found = 1;
+                    break;
+                }
+                incrementString(password);
+            }
+        }
+        v1_print_thread_result(c->index, username, password, 
+                        hash_count, getThreadCPUTime() - start_time, !found);
+        free(to_solve);
+    }
+    queue_push(q, NULL);
+    return NULL;
+}
+
+
 
 int start(size_t thread_count) {
     // TODO your code here, make sure to use thread_count!
     // Remember to ONLY crack passwords in other threads
+    q = queue_create(0);
+    char* line = NULL;
+    size_t size = 0;
+    while (1) {
+        int flag = getline(&line, &size, stdin);
+        if(flag == -1) {break;}
+        if(line[strlen(line) -1] =='\n') {line[strlen(line) -1] = '\0';}
+        queue_push(q, strdup(line));
+        count++;
+    }
+    queue_push(q, NULL);
+    free(line);
+    crackers = malloc(sizeof(cracker_t) * thread_count);
+    for (size_t i = 0; i < thread_count; i++) {
+        crackers[i].index = i+1;
+        pthread_create(&(crackers[i].tid), NULL, crack_func, crackers+i);
+    }
+    for (size_t i = 0; i < thread_count; i++) {
+        pthread_join(crackers[i].tid, NULL);
+    }
+    v1_print_summary(recover, count - recover);
+    queue_destroy(q);
+    free(crackers);
     return 0; // DO NOT change the return code since AG uses it to check if your
               // program exited normally
 }
--- password_cracker/cracker2.c
+++ password_cracker/cracker2.c
@@ -5,8 +5,140 @@
 #include "cracker2.h"
 #include "format.h"
 #include "utils.h"
+#include <stdio.h>
+#include <crypt.h>
+#include <string.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+pthread_barrier_t barrier;
+static char* username;
+static char* hash;
+static char* password;
+static char* recoverd_pw = NULL;
+static int finished = 0;
+static int found = 0;
+static int count_all = 0;
+static int num_thread;
+
+
+void *crack_func(void* id){
+    int tid = (long) id;
+    char *password_strt = calloc(10, sizeof(char));
+    while(1){
+        pthread_barrier_wait(&barrier);
+        if(finished) break;
+        strcpy(password_strt, password);
+        int count_temp = 0;
+        long start_index = 0;
+        long count = 0;
+
+        int remain = strlen(password_strt) - getPrefixLength(password_strt);
+        getSubrange(remain, num_thread, tid, &start_index, &count);
+        setStringPosition(password_strt + getPrefixLength(password_strt), start_index);
+        v2_print_thread_start(tid, username, start_index, password_strt);
+        long i = 0;
+        for (; i < count; i++){
+            struct crypt_data cdata;
+            cdata.initialized = 0;
+            char *hash_v = crypt_r(password_strt, "xx", &cdata);
+            count_temp++;
+            //success thread print result, add count and then exit loop
+            if(!strcmp(hash_v, hash)){
+                pthread_mutex_lock(&m);
+                strcpy(recoverd_pw, password_strt);
+                found = 1;
+                v2_print_thread_result(tid, count_temp, 0);
+                count_all = count_all + count_temp;
+                pthread_mutex_unlock(&m);
+                break;
+            }
+
+            if(found){
+                pthread_mutex_lock(&m);
+                v2_print_thread_result(tid, count_temp, 1);
+                count_all = count_all + count_temp;
+                pthread_mutex_unlock(&m);
+                break;
+            }
+
+            incrementString(password_strt);
+        }
+
+        pthread_barrier_wait(&barrier);
+
+        if(!found){
+            pthread_mutex_lock(&m);
+            v2_print_thread_result(tid, count_temp, 2);
+            count_all = count_all + count_temp;
+            pthread_mutex_unlock(&m);
+        }
+        pthread_barrier_wait(&barrier);
+    }
+    free(password_strt);
+    return NULL;
+}
+
 
 int start(size_t thread_count) {
+    pthread_barrier_init(&barrier, NULL, thread_count+1);
+    num_thread = thread_count;
+    pthread_t ptd[thread_count];
+
+    for (size_t i = 0; i < thread_count; i++){
+        pthread_create(ptd+i, NULL, crack_func, (void*)(i+1));
+    }
+
+    username = calloc(10, sizeof(char));
+    hash = calloc(15, sizeof(char));
+    password = calloc(10,sizeof(char));
+    recoverd_pw = calloc(16,sizeof(char));
+
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t nread = 0;
+    while (1){   
+        nread = getline(&line, &len, stdin);
+        if(nread == -1){
+            finished = 1;
+            free(hash);
+            free(password);
+            free(username);
+            free(recoverd_pw);
+            free(line);
+            pthread_barrier_wait(&barrier);
+            break;
+        }
+        
+        if (line[nread-1] == '\n') line[nread-1] = '\0';
+        sscanf(line, "%s %s %s", username, hash, password);
+        v2_print_start_user(username);
+
+        double start_time = getTime();
+        double start_cpu = getCPUTime();
+
+        pthread_barrier_wait(&barrier);
+        pthread_barrier_wait(&barrier);   
+        pthread_barrier_wait(&barrier);
+
+        double total_time = getTime() - start_time;
+        double total_cpu = getCPUTime() - start_cpu;
+
+        if(found){
+            v2_print_summary(username, recoverd_pw, count_all,total_time, total_cpu, 0);  
+        }else{
+            v2_print_summary(username, NULL, count_all, total_time, total_cpu, 1);
+        }
+        found = 0;
+        count_all = 0;
+    }
+
+    for (size_t j = 0; j < thread_count; j++) {
+      pthread_join(ptd[j], NULL);
+    }
+    pthread_mutex_destroy(&m);
+    pthread_barrier_destroy(&barrier);
     // TODO your code here, make sure to use thread_count!
     // Remember to ONLY crack passwords in other threads
     return 0; // DO NOT change the return code since AG uses it to check if your
